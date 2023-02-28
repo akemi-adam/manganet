e6879bfa7f0caea7bae5d2ee2c1b478d7a4d3aa5
Akemi Adam <90357785+akemi-adam@users.noreply.github.com>
2023-02-28 19:55:46 -0300

    Mudando páginas de autenticação para diretório apropriado

--- authors/urls.py
+++ authors/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('dashboard', views.dashboard, name = 'dashboard'),
-    path('author', views.index, name = 'author.index'),
-    path('author/<int:id>', views.show, name = 'author.show'),
+    path('author', views.index_author, name = 'author.index'),
+    path('author/<int:id>', views.show_author, name = 'author.show'),
+    path('manga', views.index_manga, name = 'manga.index'),
+    path('manga/<int:id>', views.show_manga, name = 'manga.show'),
 ]
--- authors/views.py
+++ authors/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from django.template import loader
 from django.contrib.auth.decorators import login_required
-from .models import Author
+from .models import Author, Manga
 
 @login_required
 def dashboard(request):
@@ -11,11 +12,11 @@ def dashboard(request):
     return HttpResponse(template.render())
 
 @login_required
-def index(request):
+def index_author(request):
     
     authors = Author.objects.all().values()
 
-    template = loader.get_template('index.html')
+    template = loader.get_template('author/index.html')
     
     context = {
         'authors': authors,
@@ -24,14 +25,28 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 @login_required
-def show(request, id):
+def show_author(request, id):
 
     author = Author.objects.get(id = id)
 
-    template = loader.get_template('show.html')
+    template = loader.get_template('author/show.html')
 
     context = {
         'author': author,
     }
 
     return HttpResponse(template.render(context, request))
+
+@login_required
+def index_manga(request):
+
+    mangas = Manga.objects.all()
+
+    return render(request, 'manga/index.html', {'mangas': mangas})
+
+@login_required
+def show_manga(request, id):
+
+    manga = Manga.objects.get(id = id)
+
+    return render(request, 'manga/show.html', {'manga': manga})
