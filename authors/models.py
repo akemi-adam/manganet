363816c54051907f7b25c55ba3af29292fe219d6
Akemi Adam <90357785+akemi-adam@users.noreply.github.com>
2023-02-28 20:02:02 -0300

    Criando modelo e migração da tabela de mangás no app e adicionando o novo modelo ao painel de admin

--- authors/models.py
+++ authors/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Author(models.Model):
 
@@ -6,5 +7,23 @@ class Author(models.Model):
     
     age = models.IntegerField()
 
+    user_id = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
+
     def __str__(self) -> str:
         return f"{self.name}"
+    
+class Manga(models.Model):
+
+
+    title = models.CharField(max_length=255)
+
+    number_capthers = models.IntegerField(default = 0)
+
+    author = models.ForeignKey(Author, on_delete = models.CASCADE)
+
+    synopsis = models.TextField()
+
+    publish_date = models.DateField()
+
+    def __str__(self) -> str:
+        return f'{self.title}'
