e3f339079711d662d6ce76ddf572d5456a4b6f44
Akemi Adam <90357785+akemi-adam@users.noreply.github.com>
2023-03-01 08:18:36 -0300

    Corrigindo foreign key do model Author e adicionando verbose_name às colunas dos modelos

--- authors/models.py
+++ authors/models.py
@@ -3,27 +3,26 @@ from django.contrib.auth import get_user_model
 
 class Author(models.Model):
 
-    name = models.CharField(max_length=125)
+    name = models.CharField(max_length=125, verbose_name = 'Nome')
     
-    age = models.IntegerField()
+    age = models.IntegerField(verbose_name = 'Idade')
 
-    user_id = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete = models.CASCADE, verbose_name = 'Usuário')
 
     def __str__(self) -> str:
         return f"{self.name}"
     
 class Manga(models.Model):
 
+    title = models.CharField(max_length=255, verbose_name = 'Título')
 
-    title = models.CharField(max_length=255)
+    number_capthers = models.IntegerField(default = 0, verbose_name = 'Número de capítulos')
 
-    number_capthers = models.IntegerField(default = 0)
+    author = models.ForeignKey(Author, on_delete = models.CASCADE, verbose_name = 'Author')
 
-    author = models.ForeignKey(Author, on_delete = models.CASCADE)
+    synopsis = models.TextField(verbose_name = 'Sinopse')
 
-    synopsis = models.TextField()
-
-    publish_date = models.DateField()
+    publish_date = models.DateField(verbose_name = 'Data de publicação')
 
     def __str__(self) -> str:
         return f'{self.title}'
