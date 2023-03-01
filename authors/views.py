f33aad6348a9fc6f08d261f29aa23ff817138314
Akemi Adam <90357785+akemi-adam@users.noreply.github.com>
2023-03-01 10:01:19 -0300

    Revert "Refatorando controllers"
    This reverts commit 0c03762315cfa5a6e65e2e4e017266a85db7c98e.

--- authors/views.py
+++ authors/views.py
@@ -1,24 +1,52 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.template import loader
 from django.contrib.auth.decorators import login_required
 from .models import Author, Manga
 
 @login_required
 def dashboard(request):
-    return render(request, 'dashboard.html')
+
+    template = loader.get_template('dashboard.html')
+
+    return HttpResponse(template.render())
 
 @login_required
 def index_author(request):
-    return render(request, 'author/index.html', { 'authors': Author.objects.all().values() })
+    
+    authors = Author.objects.all().values()
 
+    template = loader.get_template('author/index.html')
+    
+    context = {
+        'authors': authors,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 @login_required
 def show_author(request, id):
-    return render(request, 'author/show.html', { 'author': Author.objects.get(id = id) })
+
+    author = Author.objects.get(id = id)
+
+    template = loader.get_template('author/show.html')
+
+    context = {
+        'author': author,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 @login_required
 def index_manga(request):
-    return render(request, 'manga/index.html', { 'mangas': Manga.objects.all() })
+
+    mangas = Manga.objects.all()
+
+    return render(request, 'manga/index.html', {'mangas': mangas})
 
 @login_required
 def show_manga(request, id):
-    return render(request, 'manga/show.html', { 'manga': Manga.objects.get(id = id) })
+
+    manga = Manga.objects.get(id = id)
+
+    return render(request, 'manga/show.html', {'manga': manga})
