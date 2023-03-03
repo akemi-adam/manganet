6514ca3e8cf33b721372792ec6c3ed10ef837ff7
Akemi Adam <90357785+akemi-adam@users.noreply.github.com>
2023-03-03 11:15:14 -0300

    Tipando variavéis

--- accounts/views.py
+++ accounts/views.py
@@ -1,33 +1,38 @@
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+
+from django.db.models import Q
+
 from django.shortcuts import redirect, render
-from django.contrib.auth.models import User
+
 from django.contrib.auth import authenticate, login as auth, logout as leave
 from django.contrib.auth.decorators import user_passes_test, login_required
-from django.db.models import Q
+from django.contrib.auth.models import User
+
 from .forms import LoginForm, RegisterForm
 
 # Aux functions
 
-def guest(user) -> bool:
+def guest(user: User) -> bool:
     return not user.is_authenticated
 
 # Controllers
 
 @user_passes_test(guest, login_url = "/dashboard")
-def register(request):
+def register(request: HttpRequest) -> HttpResponse | HttpResponseRedirect | HttpResponsePermanentRedirect:
 
     if request.method == 'POST':
 
-        name = request.POST.get('username')
-        mail = request.POST.get('email')
+        name: str = request.POST.get('username')
+        mail: str = request.POST.get('email')
 
-        userAlreadyExists = User.objects.filter(Q(username = name) | Q(email = mail)).values().exists()
+        userAlreadyExists: bool = User.objects.filter(Q(username = name) | Q(email = mail)).values().exists()
 
-        form = RegisterForm(request.POST)
+        form: RegisterForm = RegisterForm(request.POST)
 
         if userAlreadyExists or not form.is_valid():
             return render(request, 'auth/register.html', { 'form': form })
 
-        user = User.objects.create_user(
+        user: User = User.objects.create_user(
             name, mail, request.POST.get('password')
         )
 
@@ -40,13 +45,13 @@ def register(request):
     return render(request, "auth/register.html", { 'form': RegisterForm() })
 
 @user_passes_test(guest, login_url = "/dashboard")
-def login(request):
+def login(request: HttpRequest):
 
     if request.method == 'POST':
 
         user = authenticate(username = request.POST.get('username'), password = request.POST.get('password'))
 
-        form = LoginForm(request.POST)
+        form: LoginForm = LoginForm(request.POST)
 
         if user != None and form.is_valid():
 
@@ -59,16 +64,14 @@ def login(request):
     return render(request, "auth/login.html", {'form': LoginForm()})
 
 @login_required
-def logout(request):
-
+def logout(request: HttpRequest) -> HttpResponseRedirect | HttpResponsePermanentRedirect:
     leave(request)
-    
     return redirect('login')
 
 @login_required
-def profile(request, id):
+def profile(request: HttpRequest, id: int) -> HttpResponse:
 
-    user = User.objects.get(id = id)
+    user: User = User.objects.get(id = id)
 
     evaluations = user.evaluation_set.all()
 
